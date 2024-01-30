3b94fd97aae57120ce5c61e6be385b47fa943392
Rishitha <137534625+RishithaDuddolla@users.noreply.github.com>
2024-01-31 01:49:01 +0530

    Updated app.http file

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ const makeServerDBconnection = async () => {
       filename: dbpath,
       driver: sqlite3.Database,
     })
-    app.listen(3000, () => {
-      console.log('server starts at 3000 port')
-    })
+    app.listen(3000)
   } catch (e) {
     console.log(e.message)
     process.exit(1)
@@ -22,49 +20,52 @@ const makeServerDBconnection = async () => {
 }
 makeServerDBconnection()
 app.get('/movies/', async (request, response) => {
-  const dbquery = `select movie_name from movie order by movie_id;`
+  const dbquery = `select movie_name from movie`
   const dbresponse = await db.all(dbquery)
   response.send(dbresponse)
 })
 app.post('/movies/', async (request, response) => {
   const dbdeets = request.body
-  const {movieId, directorId, movieName, leadActor} = dbdeets
-  const dbquery = `insert into movie (movie_id ,
+  const {directorId, movieName, leadActor} = dbdeets
+  const dbquery = `insert into movie (
 director_id,
 movie_name,  
-lead_actor) values(${movieId},${directorId},'${movieName}','${leadActor}');`
+lead_actor) values(${directorId},'${movieName}','${leadActor}');`
   const dbresponse = await db.run(dbquery)
-  response.send('Movie Successfully added')
+  response.send('Movie Successfully Added')
 })
-app.get('/movies/:movieId/', async (request, respponse) => {
-  const movieId = request.params
+app.get('/movies/:movieId/', async (request, response) => {
+  const {movieId} = request.params
   const dbquery = `select * from movie where movie_id=${movieId};`
-  const dbresponse = await db.all(dbquery)
+  const dbresponse = await db.get(dbquery)
   response.send(dbresponse)
 })
 app.put('/movies/:movieId', async (request, response) => {
-  const movieId = request.params
+  const {movieId} = request.params
   const moviedeets = request.body
   const {directorId, movieName, leadActor} = moviedeets
-  const dbquery = `update movie set (director_id=${directorId},movie_name='${movieName}',lead-actor='${leadActor}');`
+  const dbquery = `update movie set director_id=${directorId},
+  movie_name = '${movieName}',
+  lead_actor = '${leadActor}'
+  where movie_id = ${movieId};`
   const dbresponse = await db.run(dbquery)
   response.send('Movie Details Updated')
 })
 app.delete('/movies/:movieId', async (request, response) => {
-  const movieId = request.params
+  const {movieId} = request.params
   const dbquery = `delete from movie where movie_id=${movieId};`
   const dbresponse = await db.run(dbquery)
   response.send('Movie Removed')
 })
 app.get('/directors/', async (request, response) => {
-  const dbquery = `select * from director order by director_id;`
+  const dbquery = `select  director.director_id,director.director_name from movie inner join director on movie.director_id=director.director_id ;`
   const dbresponse = await db.all(dbquery)
   response.send(dbresponse)
 })
 app.get('/directors/:directorId/movies/', async (request, response) => {
-  const directorId = request.params
-  const dbquery = `select movie.movie_name from movie inner join director on movie.director_id=director.director_id;`
+  const {directorId} = request.params
+  const dbquery = `select movie_name from movie where director_id=${directorId};`
   const dbresponse = await db.all(dbquery)
-  response.send(dbquery)
+  response.send(dbresponse)
 })
 module.exports = app
